6034e1a99f395f7d3cd467c4ec3c2015036113dc
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2023-12-11 09:28:40 +0100

    Added MomentList

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MainPage from "./components/MainPage.js";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const loadNewMomentPage = () => {
+        alert("d");
+    };
+
+    return (
+        <View style={styles.container}>
+            <MainPage loadNewMomentsPage={loadNewMomentPage} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
